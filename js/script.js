b45d1ab37c5b73baae19de2e7124812ece532d2a
Emilie LSM <99273880+emilie06lsm@users.noreply.github.com>
2022-05-20 14:55:03 +0000

    edit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict"
+
+/**
+ * This function displays all numbers from 0 up to the user number
+ */
+function calculateSum() {
+  	// initialize the sum to 0
+	let sum = 0
+	// get the user number
+	let userNum = parseInt(document.getElementById('userNum').value)
+	let userNum2 = parseInt(document.getElementById('userNum2').value)
+
+  
+	// use a for loop to calculate the sum of numbers
+	for (let counter = 1; counter <= userNum2; counter++) {
+			sum = sum + userNum
+  }
+      
+  	// return the string of numbers back to html
+  	document.getElementById('display-results').innerHTML = "The product of the two numbers is" + " = " + sum 
+}
